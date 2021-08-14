4b30d3e7c6bf61935185b530599c4043cf0e36fc
medAzizRezgui <49117884+medAzizRezgui@users.noreply.github.com>
2021-08-14 13:27:42 +0200

    Single Product Page

--- snow-devil/src/App.js
+++ snow-devil/src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import NavigationBar from './components/Navigation Bar/NavigationBar';
 import Carousel from './components/Carousel/Carousel';
 import Home from './components/Home/Home';
+import Product from './components/Product/Product';
 function App() {
   return (
     <>
 
       <NavigationBar />
-      <Carousel />
-      <Home />
-
+      {/* <Carousel />
+      <Home /> */}
+      <Product />
     </>
   );
 }
---
+++ snow-devil/src/components/Product/Product.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Jacket from '../../assets/jacket1.png'
+import './Product.scss'
+function Product() {
+    const [count, setcount] = useState(1)
+
+    if (count < 1) {
+        setcount(1)
+    }
+    return (
+        <div className='container product__container'> {/* Green Box */}
+            <div className="product__image" > {/* Red Box */}
+                <img src={Jacket} alt="" />
+            </div>
+            <div className="product">   {/* Red Box */}
+                <div className="product__info">  {/* Gray Box */}
+                    <p className="product__title">haze varsity</p>
+                    <p className="product__price">$132,23</p>
+                </div>
+
+
+                <div className="product__cta">  {/* Gray Box */}
+                    <div className="row row-cols-2 row-cols-sm-3">
+                        <div className="col-6 col-sm-5">
+                            <p className="product__cta__label">size</p>
+                            <select className="product__cta__select">
+                                <option value="" className='product__cta__option'>Small</option>
+                                <option value="" className='product__cta__option'>Large</option>
+                            </select>
+                        </div>
+                        <div className="col-6 col-sm-5">
+                            <p className="product__cta__label">Color</p>
+                            <select className="product__cta__select">
+                                <option value="" className='product__cta__option'>Red</option>
+                                <option value="" className='product__cta__option'>Blue</option>
+                            </select>
+                        </div>
+                        <div className="col-6 col-sm-2">
+                            <p className="product__cta__label">Quantity</p>
+                            <div className="product__cta__count">
+                                <p className='count__minus' onClick={() => setcount(count - 1)}>-</p>
+                                <p className={'count'}>{count}</p>
+                                <p className='count__plus' onClick={() => setcount(count + 1)}>+</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Gray Box */}
+                <div className="product__buy__btns">
+                    <button className="add__to__cart">add to cart</button>
+                    <button className="buy__it__now">buy it now</button>
+                </div>
+            </div>
+            <div className="product__description">
+                <ul className="desc__list">
+                    <li>
+                        Mapped with 3M™ Thinsulate™ Insulation [40G Body / Sleeves / Hood]
+                    </li>
+                    <li>Embossed Taffeta Lining</li>
+                    <li>DRYRIDE Durashell™ 2-Layer Oxford Fabric [10,000MM, 5,000G]</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Product
